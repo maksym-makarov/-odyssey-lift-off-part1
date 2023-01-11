68ea8ce3a5ec83f6efe95902a3ee47fef4fc8caa
Maksym Makarov <maksym.makarov@smartpension.co.uk>
2023-01-11 17:06:44 +0100

    set up client

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
+
+// Apollo client
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+// An example of how to fetch data with plain JS (without React)
+// client
+//   .query({
+//     query: gql`
+//       query GetTracks {
+//         tracksForHome {
+//           id
+//           title
+//           author {
+//             id
+//             name
+//           }
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { gql, useQuery } from "@apollo/client";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
-const Tracks = () => {
-  return <Layout grid> </Layout>;
-};
 
-export default Tracks;
+const GET_TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        name
+      }
+    }
+  }
+`;
+export const Tracks = () => {
+  const { loading, error, data } = useQuery(GET_TRACKS);
+  console.log(data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+  return (
+    <Layout grid>
+      <ul>
+        {data.tracksForHome.map(({ id, title, author }) => (
+          <li key={id}>
+            <h2>{title}</h2>
+            <p>{author.name}</p>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
