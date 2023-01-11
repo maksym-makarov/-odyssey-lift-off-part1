83de60b32d6793db58da897b15ed8396312df2f1
Maksym Makarov <maksym.makarov@smartpension.co.uk>
2023-01-11 14:01:05 +0100

    set up server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,74 @@
-//TODO
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./schema");
+
+// Store in-cache
+const tracks = [
+  {
+    id: 1,
+    title: "Track_01",
+    author: {
+      id: 1,
+      name: "Mike",
+    },
+  },
+  {
+    id: 2,
+    title: "Track_02",
+    author: {
+      id: 2,
+      name: "Alex",
+    },
+  },
+  {
+    id: 3,
+    title: "Track_03",
+    author: {
+      id: 3,
+      name: "Cris",
+    },
+  },
+  {
+    id: 4,
+    title: "Track_04",
+    author: {
+      id: 1,
+      name: "Mike",
+    },
+  },
+  {
+    id: 5,
+    title: "Track_05",
+    author: {
+      id: 2,
+      name: "Alex",
+    },
+  },
+  {
+    id: 6,
+    title: "Track_06",
+    author: {
+      id: 3,
+      name: "Cris",
+    },
+  },
+];
+
+// Resolvers
+const resolvers = {
+  Query: {
+    tracksForHome: () => tracks,
+  },
+};
+
+// Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const runServer = async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  return url;
+};
+
+runServer().then((url) => console.log(`🚀  Server ready at: ${url}`));
---
+++ server/src/schema.js
@@ -0,0 +1,32 @@
+const typeDefs = `#graphql
+    type Query {
+        # query to get tracks array for the Home page
+        tracksForHome: [Track!]!
+    }
+
+    # Track type
+    type Track {
+        id: ID!
+        # the track's title
+        title: String!
+        # the track's author
+        author: Author!
+        # a link to the track's img
+        thumbnail: String
+        # the track's approx. length to complete, in minutes
+        length: Int
+        # the number of modules this track contains
+        modulesCount: Int
+    }
+
+    # Author of a complete track
+    type Author {
+        id: ID!
+        # author's name and surname
+        name: String!
+        # author's profile picture url
+        photo: String
+    }
+`
+
+module.exports = typeDefs;
